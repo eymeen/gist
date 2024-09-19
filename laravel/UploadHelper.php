c0e43c1313132bd61383de8435150faf2aa99cd1
Ayman Eid <eymeneid@gmail.com>
2024-09-20 01:07:53 +0300

    Create UploadHelper.php

---
+++ laravel/UploadHelper.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Simple class to upload files 
+ *
+ * @author Ayman Eid
+ * @version 1.0
+ * @link https://github.com/eymeen/gist/blob/main/laravel/UploadHelper.php
+ */
+namespace App\Helpers;
+
+use Image;
+use Request;
+use Illuminate\Support\Facades\File;
+
+class UploadHelper
+{
+
+    /**
+     * upload Upload Any Types of File. It's not checking the file type which may be checked before pass here in validation
+     * @param  [type] $f               [request for file or not]
+     * @param  [type] $file            [pdf file]
+     * @param  [type] $name            [filename]
+     * @param  [type] $target_location [location where file will store]
+     * @return [type]                  [filename]
+     */
+    public static function upload($file, $name, $target_location)
+    {
+        $filename = $name . '.' . $file->getClientOriginalExtension();
+        $extension = $file->getClientOriginalExtension();
+        $file->move(public_path($target_location), $filename);
+        return $filename;
+    }
+    /**
+     * upload Upload Any Types of File. It's not checking the file type which may be checked before pass here in validation
+     * @param  [type] $f               [request for file or not]
+     * @param  [type] $file            [pdf file]
+     * @param  [type] $name            [filename]
+     * @param  [type] $target_location [location where file will store]
+     * @return [type]                  [filename]
+     */
+    public static function api_upload($file, $name, $target_location)
+    {
+
+        $filename = $name . '.' . $file->getClientOriginalExtension();
+        $extension = $file->getClientOriginalExtension();
+        $file->move(public_path($target_location), $filename);
+        return $filename;
+    }
+
+
+    /**
+     * [update file]
+     * @param  [type] $f               [request for file or not]
+     * @param  [type] $file            [pdf file]
+     * @param  [type] $name            [filename]
+     * @param  [type] $target_location [location where file will store]
+     * @param  [type] $old_location    [file location which will delete]
+     * @return [type]                  [filename]
+     */
+    public static function update($file, $name, $target_location, $old_location = null)
+    {
+        if(is_null($old_location)){
+            $old_location = $target_location;
+        }
+        //delete the old notice file
+        $target_location = public_path($target_location) . '/';
+        if (File::exists($target_location . $old_location)) {
+            File::delete($target_location . $old_location);
+        }
+
+        $filename = $name . '.' . $file->getClientOriginalExtension();
+        $file->move($target_location, $filename);
+        return $filename;
+    }
+
+
+    /**
+     * [delete file]
+     * @param  [type] $location [file location that will delete]
+     * @return [type]                  [null]
+     */
+    public static function deleteFile($location)
+    {
+        if (File::exists(public_path($location))) {
+            File::delete(public_path($location));
+        }
+    }
+}
